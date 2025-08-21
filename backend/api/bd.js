2c9b9a8340d266cd8b572283eb16955427c51601
Rafael Fazion B. Dias <rafafazion9@gmail.com>
2025-08-21 18:13:09 -0300

    Descrição das funções a serem implementadas
    Coloquei como export no bd.js os nomes de algumas das funções que temos que fazer na api.

--- backend/api/bd.js
+++ backend/api/bd.js
@@ -9,3 +9,33 @@ mongoose.connect(process.env.CONNECTION_STRING, {
 .then(() => console.log("Conectado"))
 .catch(err => console.error("Erro ao conectar:", err));
 
+module.exports = {
+  login,
+  // Cliente
+  getClientePorId,
+  cadastroCliente,
+  editarCliente,
+  // Prestador
+  getTodosOsPrestadores,
+  getPrestadorPorCategoria,
+  getPrestadorPorId,
+  cadastroPrestador,
+  //editarPrestador,
+  // Categoria
+  getCategorias,
+  // Serviço
+  getServicosAcabadosDoCliente, // o historico são só serviços já terminados
+  getServicosAcabadosDoPrestador,
+  getServicosEmAndamentoDoCliente,
+  getServicosEmAndamentoDoPrestador,
+  criarServico,
+  alterarStatusDoServico,
+  // Avaliação
+  getAvaliacoesPorServico,
+  criarAvaliacao,
+  // Chat
+  getChatPorIdServico,
+  novaMensagem
+};
+
+
