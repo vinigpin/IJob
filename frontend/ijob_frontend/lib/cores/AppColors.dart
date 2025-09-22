a663f74a32416a27950a3373487c4fb897fd7bfa
Vinicius Pinheiro <vinicius.gdpinheiro@gmail.com>
2025-09-22 10:19:15 -0300

    commit tela incio

--- frontend/ijob_frontend/lib/cores/AppColors.dart
+++ frontend/ijob_frontend/lib/cores/AppColors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color vermelhoEscuro = Color(0x9F1F35);
-  static const Color vermelhoMedio = Color(0xCC2F4A);
-  static const Color vermelhoClaro = Color(0xE51934);
-  static const Color vinho = Color(0x651A1A);
+  static const Color vermelhoEscuro = Color(0xFF9F1F35);
+  static const Color vermelhoMedio = Color(0xFFCC2F4A);
+  static const Color vermelhoClaro = Color(0xFFE51934);
+  static const Color vinho = Color(0xFF651A1A);
 }
